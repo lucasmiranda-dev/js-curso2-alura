116b7fcaaf380079204a4e7d839f4e2e72c03937
Lucas Miranda <66806475+lucasmiranda-dev@users.noreply.github.com>
2024-05-02 10:00:42 -0300

    exibirMensagemInicial e reiniciarJogo
    Criamos a função exibirMensagemInicial para que não repetisse código em dois lugares, e criamos a reiniciarJogo para quando acertar ele setar os atributos do zero.

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ function exibirTextoNaTela(tag, texto) {
     campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha entre 1 e 10');
+function exibirMensagemInicial(){
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha entre 1 e 10');
+}
+
+exibirMensagemInicial();
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
@@ -36,4 +40,12 @@ function gerarNumeroAleatorio() {
 
 function limparCampo() {
     document.querySelector('input').value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    tentativas = 1;
+    limparCampo();
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
 }
