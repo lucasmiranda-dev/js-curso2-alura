5712b91b34d750010b0a79f720cd4427a67bfd88
Lucas Miranda <66806475+lucasmiranda-dev@users.noreply.github.com>
2024-05-02 09:44:27 -0300

    Update app.js
    feat: atualizamos as mensagens de tentativas, limpamos o campo input a cada tentativa e quando acerta o chute o botão novo jogo agora fica ativo

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
@@ -10,8 +11,29 @@ exibirTextoNaTela('p', 'Escolha entre 1 e 10');
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'ACERTOU!');
+        let tentativasPalavra = tentativas == 1 ? 'tentativa' : 'tentativas';
+        let mensagemTentativas = `Parabéns você acertou, o número secreto era ${numeroSecreto}! E você acertou com ${tentativas} ${tentativasPalavra}`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('h1', 'ERROU!');
+            exibirTextoNaTela('p', `O número secreto é menor que ${chute}`);
+        } else {
+            exibirTextoNaTela('h1', 'ERROU!');
+            exibirTextoNaTela('p', `O número secreto é maior que ${chute}`);
+        }
+        tentativas++;
+        limparCampo();
+    }
 }
 
 function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo() {
+    document.querySelector('input').value = '';
 }
